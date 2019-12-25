57760fdcf24152c1acd23698fa6aa6c0c0500469
Ravi Jain <jravi248@gmail.com>
2019-12-26 00:30:26 +0530

    Added Cards Animation

--- js/navigation.js
+++ js/navigation.js
@@ -2,6 +2,7 @@ const aboutNavigator = document.getElementById("nav-about");
 const portfolioNavigator = document.getElementById("nav-portfolio");
 const contactNavigator = document.getElementById("nav-contact");
 const underlines = document.getElementsByClassName("underline");
+const cards = document.getElementsByClassName("card");
 
 aboutNavigator.addEventListener("click", e => {
     underlines[0].classList.add("underline-selected");
@@ -10,6 +11,14 @@ aboutNavigator.addEventListener("click", e => {
     underlines[1].classList.add("underline-not-selected");
     underlines[2].classList.remove("underline-selected");
     underlines[2].classList.add("underline-not-selected");
+
+    cards[0].className = "card";
+    cards[1].className = "card";
+    cards[2].className = "card";
+
+    cards[0].classList.add("pos-3");
+    cards[1].classList.add("pos-4");
+    cards[2].classList.add("pos-5");
 });
 
 portfolioNavigator.addEventListener("click", e => {
@@ -19,6 +28,14 @@ portfolioNavigator.addEventListener("click", e => {
     underlines[0].classList.add("underline-not-selected");
     underlines[2].classList.remove("underline-selected");
     underlines[2].classList.add("underline-not-selected");
+
+    cards[0].className = "card";
+    cards[1].className = "card";
+    cards[2].className = "card";
+
+    cards[1].classList.add("pos-3");
+    cards[0].classList.add("pos-2");
+    cards[2].classList.add("pos-4");
 });
 
 contactNavigator.addEventListener("click", e => {
@@ -28,4 +45,12 @@ contactNavigator.addEventListener("click", e => {
     underlines[1].classList.add("underline-not-selected");
     underlines[0].classList.remove("underline-selected");
     underlines[0].classList.add("underline-not-selected");
+
+    cards[0].className = "card";
+    cards[1].className = "card";
+    cards[2].className = "card";
+
+    cards[2].classList.add("pos-3");
+    cards[1].classList.add("pos-2");
+    cards[0].classList.add("pos-1");
 });
